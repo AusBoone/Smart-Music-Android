ff704ce4b139d54b98b4dcd49429a34484198169
Suleman Lakhani <sulemanlakhani72@gmail.com>
2022-03-17 18:35:36 -0400

    fixed the button click to switch to the other activity

--- app/src/main/java/com/example/smartmusic/MainActivity.java
+++ app/src/main/java/com/example/smartmusic/MainActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+//import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.SmartMusic.MESSAGE";
@@ -16,9 +16,9 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user taps the Send button */
     public void beginButton(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+        //EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        //String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 
